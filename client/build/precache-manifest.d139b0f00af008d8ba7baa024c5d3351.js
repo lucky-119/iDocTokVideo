083829c1ffd8a2ea13b1e321e1d572d1d6e6c3de
Lakshay Arora <laarora>
2021-01-15 22:10:03 +0530

    deploy aws

---
+++ client/build/precache-manifest.d139b0f00af008d8ba7baa024c5d3351.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97ba5a93a24a1d94c11da3bf8d382094",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45a05e461adbea7eea99",
+    "url": "/static/css/main.fc2a3569.chunk.css"
+  },
+  {
+    "revision": "5cd0938f4c74f9448d9c",
+    "url": "/static/js/2.878bfb27.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.878bfb27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45a05e461adbea7eea99",
+    "url": "/static/js/main.d4c85e1f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "0a69dbfffb18dc41b25663ba66f39a82",
+    "url": "/static/media/audiooff.0a69dbff.svg"
+  },
+  {
+    "revision": "b39e8f78f10b4cfb0ddde1c247eb59d3",
+    "url": "/static/media/audioon.b39e8f78.svg"
+  },
+  {
+    "revision": "23877f23b7f8bc067673fea413c13444",
+    "url": "/static/media/callended.23877f23.svg"
+  },
+  {
+    "revision": "2520acc8e7cd50d49e545fda8d2ccaac",
+    "url": "/static/media/closeIcon.2520acc8.svg"
+  },
+  {
+    "revision": "72b5702f3796e21feb5fe3cfd7bc63df",
+    "url": "/static/media/downIcon.72b5702f.svg"
+  },
+  {
+    "revision": "286416a0323061fd3ade089bd08667e2",
+    "url": "/static/media/downloadIcon.286416a0.svg"
+  },
+  {
+    "revision": "255ea239471d4e41ffb947d45980827c",
+    "url": "/static/media/exit.255ea239.svg"
+  },
+  {
+    "revision": "a4fd5a083b28e1678f3341b295083a75",
+    "url": "/static/media/message.a4fd5a08.svg"
+  },
+  {
+    "revision": "12f2533a001d62325b37b2a7f69f6506",
+    "url": "/static/media/messageunread.12f2533a.svg"
+  },
+  {
+    "revision": "cba69d179c9d69d3f3ada38146c60680",
+    "url": "/static/media/ringing.cba69d17.mp4"
+  },
+  {
+    "revision": "3a299dcb1e5d1ff734cb719bb24c29e9",
+    "url": "/static/media/ringingImage.3a299dcb.png"
+  },
+  {
+    "revision": "3b88dda9a43479ee4e48c47ca2fd34f7",
+    "url": "/static/media/sendMessage.3b88dda9.svg"
+  },
+  {
+    "revision": "bd46e908a4dd2d70cf1414b4528190b1",
+    "url": "/static/media/shareIcon.bd46e908.svg"
+  },
+  {
+    "revision": "25c127ebb36fc2883da3f8199a2bbf80",
+    "url": "/static/media/videooff.25c127eb.svg"
+  },
+  {
+    "revision": "1fa0821d813ad3b0437de3894a3de44d",
+    "url": "/static/media/videoon.1fa0821d.svg"
+  },
+  {
+    "revision": "e5ccd67da6b9d2dd3d659cfec8ebdffd",
+    "url": "/static/media/viewIcon.e5ccd67d.svg"
+  }
+]);
--- server.js
+++ server.js
@@ -17,6 +17,12 @@ const questionnaire = require('./controllers/questionnaire.js');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+app.use(express.static("client/build"));
+
+app.get("*", (req, res) => {
+res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
 app.use('/quetionnaire',questionnaire)
 app.use('/appointment',appointment);
 app.use('/meeting',meeting);
