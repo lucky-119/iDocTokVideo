8ed3ff5344f7360c4e0490e8dda8c929ce5fe4ce
Lakshay Arora <laarora>
2021-01-15 22:36:24 +0530

    deploy aws 2

--- server.js
+++ server.js
@@ -17,15 +17,15 @@ const questionnaire = require('./controllers/questionnaire.js');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.use(express.static("client/build"));
-
-app.get("*", (req, res) => {
-res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
-
-app.use('/quetionnaire',questionnaire)
+app.use('/questionnaire',questionnaire)
 app.use('/appointment',appointment);
 app.use('/meeting',meeting);
+
+app.use(express.static("client/build"));
+
+app.get("", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 
